b8da3c10330dd504276f540ad91e24d1b680e1d5
RUneont <107897043+RUneont@users.noreply.github.com>
2022-07-13 03:54:12 +0530

    Update Instagram.py

--- Instagram.py
+++ Instagram.py
@@ -17,44 +17,18 @@ else:
     system('clear')
     system('xdg-open https://t.me/NullCyberi')
 print(white+'''
-                                    .d-
-                             .     :d-h/     -
-                      .      `+   +h```yo   /-      .
-                      -/      h. ss :N/ os `d      ::
-               .`      o+     oyy+ +MMMo /hys     /s       .
-                /:      ho  ` /N: sMMNMMy`:N+ `  +d`     :/
-                 :y-    .Ns /oh-`hMMy`sMMh`-ho+ oN.    .y/
-         `:-      -ds` o./My+d..dMMo   +MMm.`hosM/`o``od-      -:.
-           -o+`   ``ym/-m+sMy`-mMN/     /NMN:`sMy+m-/mh.`   `/o-
-             :yy: :o:oNdyMms`/NMN:       -mMN+ omMyhNs:o: :sy:`
-     ---`     `:hdo/ddhMMM+ oNMm-         .dMMo`/NMMhdd/+dh/`      ---
-      `.+o+-` /+/+dNdNMMN:`sMMh.           `hMMy`:NMMMdNdo//. `-+o+-`
-         `-ohho+ydmNMMMm-`hMMy`             `sMMh..dMMMNmy+:+yho:`
-  ..`      .:/sdmmMMMMd..dMMo                 +NMm-`hMMMNdmds/:.       ..`
-  `.:+++/:--:ohdmMMMMy`:mMN/    .-://+//:-.`   /NMN:`sMMMMmdhs/--:/+++:.`
-      `.:+syhddmMMMMs`/NMm:`-+ydmNmmdddmmNmdy+-`-mMN+`oMMMMmdddhs+:.`
-         -:+oydMMMN+`oNMm+sdNMmy+:::----::/sdNNms+mMMo`/NMMMdys+:-
-..--::::::/+shNMMN:`sMMMmNMMmhyhdhhNNNNNdhhhyyhNMMmMMMy`:mMMNhs+/::::::--..
- ``..-:/+oshdmNMm-`hMMMMMMNdhosMo.yMMMMMm./Mh+ydNMMMMMMd..dMNmdyso+/:-..``
-        `:+sydMh..dMMMMNs:-`  /Mo yMMMMMm :M/  `.:oNMMMMm-`hMdys+:.
-   ``..-:/oyhmy`:mMNsdMMd+.   `hN/./ydho.-mh    .+dMMmsNMN:`smdyo/:-..``
-.-:://+++++omo`/NMm: `/hNMNh/.``+dhs+//ohdo``./ymMNh/` -mMN+`omo+++++//::-.
-         -om+`oMMd.     -odNMNds+:+oyyys+/+sdNMNdo-     .dMMs`/do-
-       `./h:`yMMh`         -/ydmNMMNmmmNMMNNdy+-         `yMMy`-h/.`
-  ``-/oohm-.hMMs`              .-:+oosoo+/-.               sMMd..ddoo/-``
-  ...` /y.-dMNo                                             +NMm-`y+ `..`
-      +y`:NMMmsssssssssssssssssssssssssssssssssssssssssssssssmMMN/`so`
-    `so :mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm: +y`
-   `hy--:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::--sh`
-   ./////////omd+/sdohMmyMNMMMMMMMMMMMMMMMMMMMMMNMhmMh+hs/+hmo/::::::::.
-           .+o-   .`oNs-mssMdmMmMMMMMMMMMNMmMNhMssm-oNs`.   -oo.
-         `-:`     `hh. o--Nh.NoyMoNmsMymN+Mh+M-hM:-s .yh.     `::`
-                 -h:    `mh`/o Nm d//M+:m dN`++ ym` `  :h-       `
-                :+`     yy  . -M: o -M: o -M: .  sh      +/
-               ..      /s     oh    `M.    ys     o+      `-
-                      .+      h.     N`    .d      +-
-                      .      `s      h      o.      .
-                             -`      +       :
+                                 
+          
+
+
+           
+            
+           █▀ █▀▀ █ █▀▀ █▄░█ █▀▀ █░█ █▀█ █▀
+          ▄█ ██▄ █ █▄█ █░▀█ ██▄ █▄█ █▀▄ ▄█
+
+
+                         
+                           
 '''+white)
 uid = str(uuid4())
 log_head = {'User-Agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)','Accept': "*/*",'Cookie': 'missing','Accept-Encoding': 'gzip, deflate','Accept-Language': 'en-US','X-IG-Capabilities': '3brTvw==','X-IG-Connection-Type': 'WIFI','Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8','Host': 'i.instagram.com'}
